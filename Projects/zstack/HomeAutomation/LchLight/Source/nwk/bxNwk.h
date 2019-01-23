ad94dab282fd4b5fa58c79ecb983ef505e5d0b00
mo <jgb40@qq.com>
2019-01-23 10:05:11 +0800

    去除网络层,

--- Projects/zstack/HomeAutomation/LchLight/Source/nwk/bxNwk.h
+++ Projects/zstack/HomeAutomation/LchLight/Source/nwk/bxNwk.h
@@ -17,10 +17,7 @@
 // 定义网络层底层
 #define bxNWK_LowInit() //ZbInit()
 
-uint8_t bxNwkHdrLen(void);
-uint8_t *bxNwkBuildHdr(uint8_t *pDat,uint8_t fc, uint16_t dstaddr, uint8_t seq);
 int bxNwk_LowDataRequest(uint16_t dstaddr,uint8_t *dat, uint16_t len);
-int bxNwkDatareq(uint16_t dstaddr, uint8_t *dat, uint16_t len);
 
 void bxNwkInit(void);
 void bxNwk_ProcessMessageMSG( afIncomingMSGPacket_t *pInMsg );
