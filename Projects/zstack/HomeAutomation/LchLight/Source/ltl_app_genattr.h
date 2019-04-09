3a48701bb7a4424326347cccbd53286e47f8c7c8
mo <thinkgo@aliyun.com>
2019-04-09 14:54:57 +0800

    只支持三键

--- Projects/zstack/HomeAutomation/LchLight/Source/ltl_app_genattr.h
+++ Projects/zstack/HomeAutomation/LchLight/Source/ltl_app_genattr.h
@@ -41,7 +41,7 @@
 #define MANUFACTURER_NAME  "lchtime"
 
 // product identifier
-#define PRODUCT_IDENTIFIER    ( 20001 )
+#define PRODUCT_IDENTIFIER    ( 20003 )
 
 
 void ltl_GeneralBasicAttriInit(void);
